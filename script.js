f11d08f655888f0789571e3d529f5a29c75f407d
Tuafo <thiagomacenogoulart@gmail.com>
2022-06-03 18:46:57 -0300

    learn-grid-in-header

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu() {
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
 
     const nav = document.getElementById('nav');
 
     /* esse comando faz com que seja adiconada a classe 'active' no item nav quando clicar no botão menu */
     nav.classList.toggle('active'); 
 
+    /* acessibilidade */
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active)
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+    }
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
