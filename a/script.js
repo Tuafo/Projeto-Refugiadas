8e003b3785c85fd9ed07e7b21a08c05bc7d2daa1
Tuafo <92756806+Tuafo@users.noreply.github.com>
2022-06-08 13:51:26 -0300

    Delete a directory

--- a/script.js
+++
@@ -1,8 +0,0 @@
-function marcar(e) {
-    // verifica se a classe azul (estilo css que conter a imagem azul) esta no elemento
-    if (e.className == "img1") {
-      e.className = "img2";
-    } else {
-      e.className = "img1";
-    }
-  }
